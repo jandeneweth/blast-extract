957273432e629954a846e37d2929ed5d53809ff0
Jan Deneweth <jandeneweth@hotmail.com>
2022-04-02 21:39:16 +0200

    hash-sequence fixes

--- src/blast_extract/hash_sequences.py
+++ src/blast_extract/hash_sequences.py
@@ -3,24 +3,24 @@ Hash a FASTA-format file of alleles.
 """
 
 import sys
-import typing
-import Bio.SeqIO
 import hashlib
 import argparse
+import typing as t
+
+from blast_extract import seqtools
 
 
 # -- Public Methods --
 
-def iter_alleles(fh: typing.TextIO, fsep: str, field_nr: int):
-    for record in Bio.SeqIO.parse(fh, 'fasta'):
-        header = record.description
+def iter_sequences(fh: t.TextIO, fsep: str, field_nr: int):
+    for header, sequence in seqtools.parse_fasta(fh):
         fields = header.split(fsep)
         locus = fields[field_nr - 1] if len(fields) >= field_nr else None
-        sequence = str(record.seq).lower()
+        sequence = str(sequence)
         yield locus, sequence
 
 
-def get_hash_fn(algorithm: str, hash_trim: int | None):
+def get_hash_fn(algorithm: str, hash_trim: int | None) -> t.Callable[[bytes], str]:
     try:
         hasher = hashlib.new(algorithm)
     except ValueError:
@@ -40,25 +40,26 @@ def get_hash_fn(algorithm: str, hash_trim: int | None):
     return do_hash
 
 
-def run(fh: typing.TextIO, out: typing.TextIO, algorithm: str = 'md5', hash_trim: int | None = None, fsep: str = ' ', field: int = 1):
+def run(fh: t.TextIO, out: t.TextIO, algorithm: str = 'md5', hash_trim: int | None = None, fsep: str = ' ', field: int = 1):
     hash_fn = get_hash_fn(algorithm=algorithm, hash_trim=hash_trim)
-    for locus, sequence in iter_alleles(fh=fh, fsep=fsep, field_nr=field):
-        if locus is None:
+    for name, sequence in iter_sequences(fh=fh, fsep=fsep, field_nr=field):
+        if name is None:
             continue
-        hexhash = hash_fn(sequence)
-        out.write(f"{locus}\t{hexhash}\n")
+        data = sequence.upper().encode('ASCII')
+        hexhash = hash_fn(data)
+        out.write(f"{name}\t{hexhash}\n")
 
 
 # -- Run as Script --
 
 def get_argparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--algorithm', '-a', default='md5', type=str, help="The hashing algorithm used")
+    parser.add_argument('--algorithm', '-a', default='sha1', type=str, help="The hashing algorithm used")
     parser.add_argument('--hash_trim', '-t', default=None, type=int, help="If specified, the hexadecimal representation of the output hashes are trimmed to the first X characters")
     parser.add_argument('--fsep', '-s', default=' ', type=str, help="The character(s) separating fields in the fasta headers")
-    parser.add_argument('--field', '-f', default=1, type=int, help="The field in which the locus name can be found, starting at 1")
+    parser.add_argument('--field', '-f', default=1, type=int, help="The field in which the sequence name can be found")
     parser.add_argument('--out', '-o', type=argparse.FileType('w'), default=sys.stdout, help="The output destination, defaults to STDOUT")
-    parser.add_argument('ALLELES', help='The FASTA format input of alleles, defaults to STDIN', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('SEQUENCES', help='The FASTA format input of sequences, defaults to STDIN', type=argparse.FileType('r'), default=sys.stdin)
     return parser
 
 
@@ -66,7 +67,7 @@ def main(args: list[str] | None = None):
     parser = get_argparser()
     ns = parser.parse_args(args=args)
     run(
-        fh=ns.ALLELES,
+        fh=ns.SEQUENCES,
         out=ns.out,
         algorithm=ns.algorithm,
         hash_trim=ns.hash_trim,
